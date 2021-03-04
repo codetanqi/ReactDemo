9705d99944b1c0b687e277189e4e197c9e67d082
codetanqi <1528992674@qq.com>
2021-03-04 16:59:20 +0800

    商品列表静态页面完成

--- src/pages/admin/products/List.js
+++ src/pages/admin/products/List.js
@@ -1,10 +1,65 @@
-import React from 'react'
+import React from 'react';
+import { Button, Card, message, Popconfirm, Table } from 'antd'
 
+const dataSource = [
+    {
+        id: 1,
+        name: '手机',
+        price: 3699
+    },
+    {
+        id: 2,
+        name: '电脑',
+        price: 5699
+    }, {
+        id: 3,
+        name: '鼠标',
+        price: 69
+    }, {
+        id: 4,
+        name: '键盘',
+        price: 99
+    }
+];
 function List() {
+    const column = [
+        {
+            title: '序号',
+            key: 'id',
+            width: 80,
+            align: 'center',
+            render: (text, record, index) => index + 1
+        }, {
+            title: '名称',
+            dataIndex: 'name',
+        },
+        {
+            title: '价格',
+            dataIndex: 'price'
+        },
+        {
+            title: '操作',
+            render: (text, record, index) => {
+                return <div>
+                    <Button size='small' type='primary' style={{ margin: '0 0.5rem 0 0.5rem' }} >修改</Button>
+                    <Popconfirm
+                        title="确认要操作吗"
+                        onCancel={() => { message.info('取消成功'); }}
+                        onConfirm={() => { message.success('删除成功'); }}
+                        cancelText="取消"
+                        okText="确定"
+                    >
+                        <Button size='small' danger style={{ margin: '0 0.5rem 0 0.5rem' }}>删除</Button></Popconfirm>
+                </div>
+            }
+        }
+    ]
+
+
     return (
-        <div>
-            <h1>商品列表</h1>
-        </div>
+        <Card title="商品列表" extra={<Button type="primary" size="small">新增</Button>}>
+            <Table columns={column} bordered dataSource={dataSource} ></Table>
+        </Card>
     )
 }
 
