da913bb2a309c4885edaa4bcf89390fd600aea76
codetanqi <1528992674@qq.com>
2021-03-04 14:13:48 +0800

    完成路由配置

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import logo from './logo.svg';
 import { Button } from 'antd';
 import 'antd/dist/antd.css'
 import './App.css';
-
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { adminRoutes } from './routes'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button type='primary' >1231</Button>
-      </header>
+      <h1>这是App组件</h1>
+      <Switch>
+        {adminRoutes.map(route => {
+          return <Route key={route.path} path={route.path} exact={route.exact} render={routeProps => {
+            return <route.component {...routeProps} />
+          }}></Route>
+        })}
+        <Redirect to='/404' />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import { mainRoutes } from './routes'
 import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
@@ -11,6 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
+        <Route path='/admin' render={routeProps => <App {...routeProps} />}></Route>
         {mainRoutes.map(route => {
           console.log(route)
           return <Route key={route.path} {...route} />
