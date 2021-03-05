052e177c10136a9b03ad3489082db9fa625acdcf
codetanqi <1528992674@qq.com>
2021-03-05 13:43:38 +0800

    登录跳转路由完成

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,57 +1,70 @@
 import React from 'react'
 import { Form, Input, Button, message } from 'antd';
-import bgImg from '../components/Frame/logo.png'
+import bgImg from '../bgPng/bgImg.jpg'
 
-function Login(props) {
+class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
 
-    const onFinish = (props) => {
+        }
     }
-    const onFinishFailed = () => {
+
+    onFinish = () => {
+        this.props.history.push('/admin/dashboard')
+    }
+    onFinishFailed = () => {
 
     }
-    return (
-        <div style={{
-            width: '400px',
-            height: '300px',
-            padding: '1em',
-            textAlign: 'center',
-            boxShadow: '1px 2px 3px 2px #ddd',
-            borderRadius: '5px',
-            marginLeft: '-200px',
-            marginTop: '-150px',
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-        }}>
-            <h1>登录</h1>
-            <Form
-                name="basic"
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-            >
-                <Form.Item
-                    label="Username"
-                    name="username"
-                    rules={[{ required: true, message: '请输入账户名!' }]}
-                >
-                    <Input />
-                </Form.Item>
 
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: '请输入密码!' }]}
-                >
-                    <Input.Password />
-                </Form.Item>
-                <Form.Item >
-                    <Button type="primary" htmlType="submit">
-                        Submit
-        </Button>
-                </Form.Item>
-            </Form>
-        </div>
-    )
+    render() {
+        return (
+            <div className='wrap' style={{ width: '100%', height: '100%', backgroundColor: "linen" }}>
+                <div style={{
+                    width: '400px',
+                    height: '300px',
+                    padding: '1em',
+                    textAlign: 'center',
+                    boxShadow: '1px 2px 3px 2px #ddd',
+                    borderRadius: '5px',
+                    marginLeft: '-200px',
+                    marginTop: '-150px',
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                }}>
+                    <h1>登录</h1>
+                    <Form
+                        name="basic"
+                        onFinish={this.onFinish}
+                        onFinishFailed={this.onFinishFailed}
+                    >
+                        <Form.Item
+                            label="Username"
+                            name="username"
+                            rules={[{ required: true, message: '请输入账户名!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: '请输入密码!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+                        <Form.Item >
+                            <Button type="primary" htmlType="submit">
+                                Submit
+            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+            </div>
+        )
+    }
+
 }
 
 export default Login
